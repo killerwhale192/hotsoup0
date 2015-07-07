a5f7246c25aa756582f142226668db203776738b
killerwhale192 <jinqyi.lee@gmail.com>
2015-07-06 22:23:39 -0400

    created todos

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,35 @@
+class TodosController < ApplicationController
+
+	def index
+		#collections - bunch of todos
+
+	end
+
+	def create
+		#end point for Post: create a new todo and save into the database
+	end
+
+	def new 
+		#see that page to create - where you see the page to create
+	end
+
+	def edit
+		#after I create - link to edit to edit that thing
+		#template to update the database
+
+	end
+
+	def show
+		#to show a single todo
+	end
+
+	def destroy
+		#to delete a single todo
+	end
+
+	def update
+		#
+
+	end
+
+end
